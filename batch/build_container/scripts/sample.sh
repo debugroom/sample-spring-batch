f6cb7167a7fd09722c4d7df3ea6167c52eb3a59b
Kohei Kawabata <kohei.kawabata@mac.com>
2019-06-11 10:05:07 +0900

    Fix error in dockerfile and sample script.

--- batch/build_container/scripts/sample.sh
+++ batch/build_container/scripts/sample.sh
@@ -10,4 +10,4 @@ ERRORLOG_FILENAME=/logs/app-error-$DATETIME.log
 touch $APPLOG_FILENAME
 touch $ERRORLOG_FILENAME
 
-java -jar /user/local/sample-spring-batch/batch/target/batch-0.0.1-SNAPSHOT.jar 1>> $APPLOG_FILENAME 2>> $ERRORLOG_FILENAME
+java -jar /usr/local/sample-spring-batch/batch/target/batch-0.0.1-SNAPSHOT.jar 1>> $APPLOG_FILENAME 2>> $ERRORLOG_FILENAME
